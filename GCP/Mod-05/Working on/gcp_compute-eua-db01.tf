76d914b68906b3aff06121229ad83159ca57a93c
Alan Silva <alan.ocs@gmail.com>
2021-08-31 00:47:13 -0300

    Ansible

--- GCP/Mod-05/Working on/gcp_compute-eua-db01.tf
+++ GCP/Mod-05/Working on/gcp_compute-eua-db01.tf
@@ -3,16 +3,6 @@ resource "google_compute_instance" "eua-db01" {
   machine_type = var.gcp_instance_type
   zone = var.gcp_zone
 
- # metadata_startup_script = <<SCRIPT
- #     sudo apt update 
- #     sudo apt-get -y install wget
- #     wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb
- #     dpkg -i mysql-apt-config_0.8.13-1_all.deb
- #     sudo apt install mysql-server -y
- #     sudo mysqladmin -u root password welcome1
- #     sudo systemctl restart mysql.service
- #     SCRIPT
-
   boot_disk {
     initialize_params {
       image = var.gcp_disk_image
@@ -25,26 +15,33 @@ resource "google_compute_instance" "eua-db01" {
     }
   }
 
-  provisioner "remote-exec" {
-      connection {
-      host = "eua-db01"
-      type = "ssh"
-      port = 22
-      user = "root"
-      timeout = "500s"
-      agent = "false"
-      private_key = file("/home/ubuntu/Documents/TCB/GCP/05/root.pub")
-      }
-    inline = [
-    "sudo apt update", 
-    "sudo apt-get -y install wget", 
-    "wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb", 
-    "dpkg -i mysql-apt-config_0.8.13-1_all.deb", 
-    "sudo apt install mysql-server -y", 
-    "sudo mysqladmin -u root password welcome1", 
-    "sudo systemctl restart mysql.service"
-      ]
+  metadata = {
+  ssh-keys = "${var.username}:${file("${var.key_priv}")}"
   }
 
+  connection {
+    host = self.network_interface.0.access_config.0.nat_ip
+    type = "ssh"
+    user = var.username
+    agent = "false"
+    private_key = file(var.key_priv)
+    }
 
+
+  provisioner "local-exec" {
+  command = "ansible-playbook -i '${self.network_interface.0.access_config.0.nat_ip},' --private-key ${var.key_priv} mysql.yml"
   }
+ 
+#    provisioner "remote-exec" {
+
+#     inline = [
+#     "sudo apt update", 
+#     "sudo apt-get -y install wget", 
+#     "wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb", 
+#     "dpkg -i mysql-apt-config_0.8.13-1_all.deb", 
+#     "sudo apt install mysql-server -y", 
+#     "sudo mysqladmin -u root password welcome1", 
+#     "sudo systemctl restart mysql.service"
+#       ]
+#   }
+}
