e447bf628967a1828142d414b9a1f8a08418e16e
Alan Silva <alan.ocs@gmail.com>
2021-11-18 19:30:09 -0300

    Updates

--- AWS/Módulo-02/localexec.tf
+++ AWS/Módulo-02/localexec.tf
@@ -1,9 +1,9 @@
-resource "null_resource" "Run_Python_Script" {
+resource "null_resource" "Run_Ansible_Script" {
 
  provisioner "local-exec" {
 
-    # command = "ansible-playbook createusers.yml"
-    command = "python3 createusers.py"
+     command = "ansible-playbook createusers.yml"
+    # command = "python3 createusers.py"
 
   }
   depends_on = [aws_iam_group_policy_attachment.CloudAdmin-AdministratorAccess, aws_iam_group_policy_attachment.DBA-RDSFullAccess, aws_iam_group_policy_attachment.LinuxAdmin-EC2FullAccess, aws_iam_group_policy_attachment.RedesAdmin-VPCFullAccess, aws_iam_group_policy_attachment.Estagiarios-ReadOnly]
